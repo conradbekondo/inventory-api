ec72eeeaee2731c3a55c5486f72f354430727b74
IlorsonDel <ethanetidal@gmail.com>
2024-04-26 22:13:16 +0100

    chore: Endpoint-for-dto-and-quantity-return-v3

--- src/main/java/ca/quickdo/springintro/controller/InventoryController.java
+++ src/main/java/ca/quickdo/springintro/controller/InventoryController.java
@@ -7,17 +7,10 @@ import ca.quickdo.springintro.repository.ProductsRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import net.bytebuddy.asm.Advice;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
